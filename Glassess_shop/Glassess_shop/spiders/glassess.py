1dde9ef5a33c0cc7a9ab0b257661c46fe96d7086
izharabbasi <izharabbasi103@gmail.com>
2020-07-22 15:49:16 +0500

    first page scraper

--- Glassess_shop/Glassess_shop/spiders/glassess.py
+++ Glassess_shop/Glassess_shop/spiders/glassess.py
@@ -11,12 +11,18 @@ class GlassessSpider(scrapy.Spider):
         yield scrapy.Request(url='https://www.glassesshop.com/bestsellers', callback=self.parse ,headers={
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36 Edg/84.0.522.40'
         })
+    
+    def clean_output(self, data):
+        try:
+            return data.strip()
+        except AttributeError:
+            return ''
 
     def parse(self, response):
         glassess_main = response.xpath("//div[@id='product-lists']/div")
         for glasses in glassess_main:
             yield {
-                'Prouct_name': glasses.xpath(".//div[@class='p-title-block']/div[2]/div/div/div/a/text()").get(),
+                'Prouct_name': self.clean_output(glasses.xpath(".//div[@class='p-title']/a/text()").get()),
                 'price': glasses.xpath(".//div[@class='p-title-block']/div[2]/div/div[2]/div/div/span/text()").get(),
                 'product_url': glasses.xpath(".//div[@class='product-img-outer']/a/@href").get(),
                 'image_url': glasses.xpath(".//div[@class='product-img-outer']/a/img[1]/@src").get()
