0215c0b36b4dae4a775678d20bc387ee2794a4d9
izharabbasi <izharabbasi103@gmail.com>
2020-08-12 15:30:06 +0500

    add selenium

--- samtrygg/samtrygg/settings.py
+++ samtrygg/samtrygg/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'samtrygg.spiders'
 #USER_AGENT = 'samtrygg (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -27,9 +27,9 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 1
 # The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
+CONCURRENT_REQUESTS_PER_DOMAIN = 2
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
@@ -52,9 +52,9 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'samtrygg.middlewares.SamtryggDownloaderMiddleware': 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy_selenium.SeleniumMiddleware': 800
+}
 
 # Enable or disable extensions
 # See https://doc.scrapy.org/en/latest/topics/extensions.html
@@ -88,3 +88,9 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+from shutil import which
+
+SELENIUM_DRIVER_NAME = 'chrome'
+SELENIUM_DRIVER_EXECUTABLE_PATH = which('chromedriver')
+SELENIUM_DRIVER_ARGUMENTS=['--headless']  # '--headless' if using chrome instead of firefox
--- samtrygg/samtrygg/spiders/property_scraper.py
+++ samtrygg/samtrygg/spiders/property_scraper.py
@@ -7,5 +7,31 @@ class PropertyScraperSpider(scrapy.Spider):
     allowed_domains = ['www.samtrygg.se']
     start_urls = ['https://www.samtrygg.se/']
 
-    def parse(self, response):
-        pass
+    base_url = 'https://www.samtrygg.se/RentalObject/NewSearch'
+
+    headers = {
+        'User-Agent' :'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36 Edg/84.0.522.58',
+        'cookie': '.ASPXANONYMOUS=b_XM7x-n1gEkAAAAZTJjNTY0ZDQtMzVhNi00ODFmLTkzZDQtM2Y1MmRhZjE3YTI4LPix5tdrvKwwcnJwqEeXWc3RWAtgyd28urCjqHm4Reo1; _ga=GA1.2.477490575.1597225871; _gid=GA1.2.1381664640.1597225871; _vwo_uuid_v2=D1C81876362519D5FF49EFB7A628F972C|c392565894d10efecc264de768152182; _hjid=ad964e22-eb63-46d2-83c0-f73ea4a5c04a; _hjIncludedInPageviewSample=1; __ca__chat=sw9c7oGr8scT; cto_bundle=FO_dhV9vYlU4S2xVbHZic2llRmFpdkVYdmR5Q044MnBIdXp2ZTVIZzVPMmZaVE9mZHBTb05UcUxvU2V5TVVtT0Q0S3FwczNJaTBBUEpwdXhZanNrTXFZMVpwJTJCblIlMkJGQzNQSURGS3V1VFZuV0JERk5QN1FOM2RBb0ZvMzBnZ2lKbVhjS282dnIyUERGU0hNVXJOeUFIYU9DUThBJTNEJTNE; mp_2a6c1441f45b68d8dac010f9938a8688_mixpanel=%7B%22distinct_id%22%3A%20%22173e214abc72e6-0848696fb7df54-7e647c66-e1000-173e214abc8749%22%2C%22%24device_id%22%3A%20%22173e214abc72e6-0848696fb7df54-7e647c66-e1000-173e214abc8749%22%2C%22%24initial_referrer%22%3A%20%22%24direct%22%2C%22%24initial_referring_domain%22%3A%20%22%24direct%22%7D'
+    }
+
+    def start_requests(self):
+        yield scrapy.Request(
+            url=self.base_url,
+            headers=self.headers,
+            callback=self.parse_links
+        )
+
+    def parse_links(self, res):
+        cards = res.xpath("//div[@class='owl-carousel owl-theme show-nav-hover']/div/a")
+
+        for card in cards:
+            link = card.xpath(".//@href").get()
+            
+            yield scrapy.Request(
+                url=link,
+                headers=self.headers,
+                callback=self.parse_listings
+            )
+    
+    def parse_listings(self,res):
+        print('\n\n\nRESPONE' , res.text)
