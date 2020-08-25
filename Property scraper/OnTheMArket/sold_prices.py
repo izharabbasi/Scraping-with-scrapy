8184270e43c3af80c4f329e452b884ff522a7194
izharabbasi <izharabbasi103@gmail.com>
2020-08-26 00:31:18 +0500

    complete scraper

--- Property scraper/OnTheMArket/sold_prices.py
+++ Property scraper/OnTheMArket/sold_prices.py
@@ -40,7 +40,83 @@ class SoldPrices(scrapy.Spider):
     def start_requests(self):
         for postcode in self.postcodes:
             next_postcode = self.base_url + urllib.parse.urlencode(self.params) + '&sold-prices-input=' + postcode
-            print(next_postcode)
+            yield scrapy.Request(
+                url=next_postcode,
+                headers=self.headers,
+                meta={
+                    'postcode' : postcode
+                },
+                callback=self.parse
+            )
+            break
+    
+    def parse(self,response):
+        postcode = response.meta.get('postcode')
+
+        listings = response.xpath("//div[@class='outcode-street-list column_quarter']/ul/li")
+        for l in listings:
+            link = l.xpath(".//a/@href").get()
+            street = l.xpath(".//a/text()").get()
+            yield response.follow(
+                url=link,
+                headers=self.headers,
+                meta={
+                    'postcode' : postcode,
+                    'street' : street
+                },
+                callback=self.parse_data
+            )
+        
+        next_page = response.xpath("//a[@title='Next page']/@href").get()
+        try:
+            if next_page:
+                yield response.follow(
+                    url=next_page,
+                    headers=self.headers,
+                    meta={
+                        'postcode' : postcode,
+                        'street' : street
+                    },
+                    callback=self.parse
+                )
+        except AttributeError:
+            pass
+
+
+
+    def parse_data(self,response):
+        postcode = response.meta.get('postcode')
+        street = response.meta.get('street')
+        cards = response.xpath("//div[@class='results list-tabcontent primary-col']/div/div")
+        for card in cards:
+            yield {
+                'postcode': postcode,
+                'street' : street,
+                'address' : card.xpath("./h2/text()").get(),
+                'price' : card.xpath(".//ul/li[1]/span[1]/text()").getall(),
+                'description' : card.xpath(".//ul/li[1]/span[2]/text()").get(),
+                'date' : card.xpath(".//ul/li[1]/span[3]/text()").get()
+
+            }
+        next_page = response.xpath("//a[@title='Next page']/@href").get()
+        
+        try:
+            if next_page:
+                yield response.follow(
+                    url=next_page,
+                    headers=self.headers,
+                    meta={
+                        'postcode' : postcode,
+                        'street' : street
+                    },
+                    callback=self.parse_data
+                )
+        except AttributeError:
+            pass
+
+
+
+
 #run scraper
 process = CrawlerProcess()
 process.crawl(SoldPrices)
