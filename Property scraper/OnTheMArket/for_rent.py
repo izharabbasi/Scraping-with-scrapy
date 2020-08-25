1a8e5797cbd10f853d22a48b5e2306bd1ea8f513
izharabbasi <izharabbasi103@gmail.com>
2020-08-25 18:32:22 +0500

    complete for rent scraper

---
+++ Property scraper/OnTheMArket/for_rent.py
@@ -0,0 +1,123 @@
+import scrapy
+from scrapy.crawler import CrawlerProcess
+import urllib
+import json
+import re
+
+
+def cleanUp(inputString):
+    if inputString:
+        return re.sub('[\n\\n\n\n''                      ]','',inputString).strip()
+
+
+class forRent(scrapy.Spider):
+    name = 'Rent'
+
+    base_url = 'https://www.onthemarket.com/to-rent/property/'
+
+    params = {
+        'page': '0',
+        'radius': '3.0'
+    }
+    
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36 Edg/84.0.522.63'
+    }
+
+    custom_settings = {
+        'FEED_FORMAT' : 'csv',
+        'FEED_URI' : 'Sale_data.csv',
+        #CONCURRENT_REQUESTS_PER_DOMAIN': 2,
+        #'DOWNLOAD_DELAY': 1
+    }
+
+    postcodes = []
+
+    def __init__(self):
+        content = ''
+
+        with open(r'C:\Users\izhar\Projects\Property scraper\Right Move\postcode.json', 'r') as f:
+            for line in f.read():
+                content += line
+                
+            content = json.loads(content)
+            
+            for item in content:
+                self.postcodes.append(item['postcode'])
+            
+    def start_requests(self):
+        for postcode in self.postcodes:
+            next_postcode = self.base_url + postcode.lower() + '/?' + urllib.parse.urlencode(self.params)
+            yield scrapy.Request(
+                url=next_postcode,
+                headers=self.headers,
+                callback=self.parse,
+                meta={
+                    'postcode': postcode
+                }
+            )
+            break
+    
+    def parse(self, response):
+        postcode = response.meta.get('postcode')
+
+        cards = response.xpath("//ul[@id='properties']/li")
+        for card in cards:
+            title = card.xpath('.//div[3]/p[2]/span[1]/a/text()').get()
+            address = card.xpath('.//div[3]/p[2]/span[2]/a/text()').get()
+            price = card.xpath('.//div[3]/p[1]/a/text()[1]').get()
+            link = card.xpath(".//div[3]/p[2]/span[2]/a/@href").get()
+            
+            yield response.follow(
+                url=link,
+                headers=self.headers,
+                meta={
+                    'title': title,
+                    'address': address,
+                    'price': price,
+                    'postcode':postcode
+                },
+                callback=self.parse_listings
+            )
+        next_page = response.xpath("//a[@title='Next page']/@href").get()
+        try:
+            if next_page:
+                yield response.follow(
+                    url = next_page,
+                    headers = self.headers,
+                    meta = {
+                        'postcode': postcode
+                    },
+                    callback = self.parse
+                )
+        except AttributeError:
+            pass
+    
+    def parse_listings(self,response):
+        postcode = response.meta.get('postcode')
+        title = response.meta.get('title')
+        address = response.meta.get('address')
+        price = response.meta.get('price')
+
+        yield {
+            'postcode':postcode,
+            'id': response.url.split("https://www.onthemarket.com/details/")[-1].split('/')[0],
+            'title': title,
+            'address': address,
+            'price' : price,
+            'agent' : response.xpath('//*[@id="property-actions"]/div/div[1]/div/a/div[2]/h2/text()').get(),
+            'agent_address' : response.xpath('//*[@id="property-actions"]/div/div[1]/div/a/div[2]/div/text()').get().strip(),
+            'agent_phone': response.xpath('//*[@id="property-actions"]/div/div[1]/div/div/div[2]/text()').get(),
+            'features' : str(response.xpath("//ul[@class='property-features']/li/descendant::text()").getall()).strip(),
+            'description' : cleanUp(str(response.xpath("//div[@id='description-text']/descendant::text()").getall())).replace('\\n','').replace('','')
+
+        }
+
+    
+
+
+#run scraper
+
+process = CrawlerProcess()
+process.crawl(forRent)
+process.start()
