78e79093797add27eb4cf8fb657be454399513a2
izharabbasi <izharabbasi103@gmail.com>
2020-08-27 14:52:11 +0500

    git push

--- Property scraper/TradeMe/for_rent.py
+++ Property scraper/TradeMe/for_rent.py
@@ -6,11 +6,7 @@ import re
 
 class ForSale(scrapy.Spider):
     name = 'for_sale'
-    location = 'waikato'
-    district = 'hamilton'
-    suburbs = 'bader'
-
-    base_url = 'https://www.trademe.co.nz/a/property/residential/rent/'
+   
 
     headers = {
         'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36 Edg/84.0.522.63'
@@ -24,9 +20,8 @@ class ForSale(scrapy.Spider):
     }
 
     def start_requests(self):
-        url = self.base_url + self.location + '/' + self.district + '/' +self.suburbs
         yield scrapy.Request(
-            url= url,
+            url= 'https://www.trademe.co.nz/a/property/residential/rent/search?page=5',
             headers= self.headers,
             callback= self.parse
         )
@@ -41,26 +36,27 @@ class ForSale(scrapy.Spider):
                 headers=self.headers,
                 callback=self.parse_listing
             )
-        next_page = response.xpath("//a[contains(text(),'Next')]/@href").get()
-        if next_page:
-            yield response.follow(
-                url=next_page,
-                headers=self.headers,
-                callback=self.parse
-            )
+        # next_page = response.xpath("//a[contains(text(),'Next')]/@href").get()
+        # if next_page:
+        #     yield response.follow(
+        #         url=next_page,
+        #         headers=self.headers,
+        #         callback=self.parse
+        #     )
         
     def parse_listing(self, response):
         
         yield {
-            'link': response.url,
-            'country': 'New Zealand',
-            'listed': response.xpath("//div[@class='tm-property-listing-body__date p-secondary-copy']/text()").get(),
-            'address': response.xpath('/html/body/trade-me/div[1]/main/div/tm-property-listing/div/tm-property-listing-body/div/section[1]/tg-row/tg-col/h1/text()').get(),
-            'bedrooms': response.xpath("//ul[@class='tm-property-listing-attributes__tag-list']/li[1]/tm-property-listing-attribute-tag/tg-tag/span/div/text()").get(),
-            'bathrooms': response.xpath("//ul[@class='tm-property-listing-attributes__tag-list']/li[2]/tm-property-listing-attribute-tag/tg-tag/span/div/text()").get(),
-            'car_spaces' : response.xpath("(//td[@data-th='Value'])[3]/text()").get(),
-            'agency' : response.xpath("//h3[@class='pt-agency-summary__agency-name']/text()").get(),
-            'agent': response.xpath("//h3[@class='pt-agent-summary__agent-name']/text()").get()
+            'Address': response.xpath('/html/body/trade-me/div[1]/main/div/tm-property-listing/div/tm-property-listing-body/div/section[1]/tg-row/tg-col/h1/text()').get(),
+            'Bedrooms': response.xpath("//ul[@class='tm-property-listing-attributes__tag-list']/li[1]/tm-property-listing-attribute-tag/tg-tag/span/div/text()").get(),
+            'Bathrooms': response.xpath("//ul[@class='tm-property-listing-attributes__tag-list']/li[2]/tm-property-listing-attribute-tag/tg-tag/span/div/text()").get(),
+            'Listing Title / Heading': response.xpath("//h2[@class='tm-property-listing-body__title p-h1']/text()").get(),
+            'Date Property First Listed': response.xpath("(//div[@_ngcontent-frend-c46])[2]/text()").get(),
+            'Agency Name' : response.xpath("//h3[@class='pt-agency-summary__agency-name']/text()").get(),
+            'Agent Name': response.xpath("//h3[@class='pt-agent-summary__agent-name']/text()").get(),
+            'Listing Link': response.url,
+            'Contact Number': response.xpath("//a[@_ngcontent-frend-c97]/@href").get(),
+            'Property Description' : response.xpath("//div[@class='tm-markdown']/descendant::node()/text()").getall()
         }
 
 
